55cef8995e5390f6caad348ee62fcf42e43f89ab
SrGetulio <marcosgetulio22@gmail.com>
2025-02-09 15:46:43 +0000

    arquivo completo

---
+++ SuperTrunfo-Iniciante.c
@@ -0,0 +1,451 @@
+#include <stdio.h>
+
+int main () {
+
+    printf("Jogo Super Trunfo - Iniciante \n"); 
+
+    printf ("Países!\n");
+
+    //Declaração de variáveis
+    float populacaoA01, populacaoA02, populacaoA03, populacaoA04;
+    float areaA01, areaA02, areaA03, areaA04;
+    float pibA01, pibA02, pibA03, pibA04;
+    int turisticosA01, turisticosA02, turisticosA03, turisticosA04;
+
+    float populacaoB01, populacaoB02, populacaoB03, populacaoB04;
+    float areaB01, areaB02, areaB03, areaB04;
+    float pibB01, pibB02, pibB03, pibB04;
+    int turisticosB01, turisticosB02, turisticosB03, turisticosB04;
+
+    float populacaoC01, populacaoC02, populacaoC03, populacaoC04;
+    float areaC01, areaC02, areaC03, areaC04;
+    float pibC01, pibC02, pibC03, pibC04;
+    int turisticosC01, turisticosC02, turisticosC03, turisticosC04;
+
+    float populacaoD01, populacaoD02, populacaoD03, populacaoD04;
+    float areaD01, areaD02, areaD03, areaD04;
+    float pibD01, pibD02, pibD03, pibD04;
+    int turisticosD01, turisticosD02, turisticosD03, turisticosD04;
+
+    float populacaoE01, populacaoE02, populacaoE03, populacaoE04;
+    float areaE01, areaE02, areaE03, areaE04;
+    float pibE01, pibE02, pibE03, pibE04;
+    int turisticosE01, turisticosE02, turisticosE03, turisticosE04;
+
+    float populacaoF01, populacaoF02, populacaoF03, populacaoF04;
+    float areaF01, areaF02, areaF03, areaF04;
+    float pibF01, pibF02, pibF03, pibF04;
+    int turisticosF01, turisticosF02, turisticosF03, turisticosF04;
+
+    float populacaoG01, populacaoG02, populacaoG03, populacaoG04;
+    float areaG01, areaG02, areaG03, areaG04;
+    float pibG01, pibG02, pibG03, pibG04;
+    int turisticosG01, turisticosG02, turisticosG03, turisticosG04;
+
+    float populacaoH01, populacaoH02, populacaoH03, populacaoH04;
+    float areaH01, areaH02, areaH03, areaH04;
+    float pibH01, pibH02, pibH03, pibH04;
+    int turisticosH01, turisticosH02, turisticosH03, turisticosH04;
+
+    //A01
+    printf("Pais A01\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoA01);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaA01);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibA01);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosA01);
+    
+    //A02
+    printf("Pais A02\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoA02);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaA02);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibA02);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosA02);
+
+   //A03
+    printf("Pais A03\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoA03);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaA03);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibA03);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosA03);
+
+    //A04
+    printf("Pais A04\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoA04);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaA04);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibA04);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosA04);
+
+    //B01
+    printf("Pais B01\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoB01);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaB01);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibB01);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosB01);
+
+    //B02
+    printf("Pais B02\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoB02);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaB02);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibB02);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosB02);
+
+    //B03
+    printf("Pais B03\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoB03);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaB03);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibB03);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosB03);
+
+    //B04
+    printf("Pais B04\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoB04);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaB04);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibB04);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosB04);
+
+    //C01
+    printf("Pais C01\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoC01);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaC01);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibC01);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosC01);
+
+    //C02
+    printf("Pais C02\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoC02);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaC02);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibC02);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosC02);
+
+    //C03
+    printf("Pais C03\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoC03);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaC03);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibC03);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosC03);
+
+    //C04
+    printf("Pais C04\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoC04);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaC04);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibC04);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosC04);
+
+    //D01
+    printf("Pais D01\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoD01);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaD01);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibD01);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosD01);
+
+
+    //D02
+    printf("Pais D02\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoD02);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaD02);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibD02);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosD02);
+
+    //D03
+    printf("Pais D03\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoD03);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaD03);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibD03);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosD03);
+
+    //D04
+    printf("Pais D04\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoD04);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaD04);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibD04);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosD04);
+
+    //E01
+    printf("Pais E01\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoE01);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaE01);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibE01);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosE01);
+
+
+    //E02
+    printf("Pais E02\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoE02);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaE02);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibE02);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosE02);
+
+    //E03
+    printf("Pais E03\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoE03);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaE03);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibE03);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosE03);
+
+    //E04
+    printf("Pais E04\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoE04);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaE04);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibE04);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosE04);
+
+    //F01
+    printf("Pais F01\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoF01);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaF01);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibF01);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosF01);
+
+    //F02
+    printf("Pais F02\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoF02);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaF02);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibF02);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosF02);
+
+    //F03
+    printf("Pais F03\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoF03);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaF03);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibF03);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosF03);
+
+    //F04
+    printf("Pais F04\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoF04);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaF04);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibF04);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosF04);
+
+
+    //G01
+    printf("Pais G01\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoG01);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaG01);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibG01);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosG01);
+
+    //G02
+    printf("Pais G02\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoG02);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaG02);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibG02);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosG02);
+
+    //G03
+    printf("Pais G03\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoG03);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaG03);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibG03);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosG03);
+
+    //G04
+    printf("Pais G04\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoG04);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaG04);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibG04);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosG04);
+
+    //H01
+    printf("Pais H01\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoH01);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaH01);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibH01);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosH01);
+
+    //H02
+    printf("Pais H02\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoH02);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaH02);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibH02);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosH02);
+
+    //H03
+    printf("Pais H03\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoH03);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaH03);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibH03);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosH03);
+
+    //H04
+    printf("Pais H04\n");
+    printf("Qual a População:\n");
+    scanf("%f", &populacaoH04);
+    printf("Digite a Área:\n");
+    scanf("%f", &areaH04);
+    printf("Qual o PIB?\n");
+    scanf("%f", &pibH04);
+    printf("Quantos Pontos Turisticos?\n");
+    scanf("%d", &turisticosH04);
+
+
+    //Resultados: 
+    printf(" - Resultado A01 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoA01, areaA01, pibA01,turisticosA01);
+    printf(" - Resultado A02 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoA02, areaA02, pibA02,turisticosA02);
+    printf(" - Resultado A03 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoA03, areaA03, pibA03,turisticosA03);
+    printf(" - Resultado A04 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoA04, areaA04, pibA04,turisticosA04);  
+
+    printf(" - Resultado B01 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoB01, areaB01, pibB01,turisticosB01);
+    printf(" - Resultado B02 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoB02, areaB02, pibB02,turisticosB02);
+    printf(" - Resultado B03 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoB03, areaB03, pibB03,turisticosB03);
+    printf(" - Resultado B04 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoB04, areaB04, pibB04,turisticosB04);
+
+    printf(" - Resultado C01 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoC01, areaC01, pibC01,turisticosC01);
+    printf(" - Resultado C02 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoC02, areaB02, pibC02,turisticosC02);
+    printf(" - Resultado C03 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoC03, areaC03, pibC03,turisticosC03);
+    printf(" - Resultado C04 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoC04, areaC04, pibC04,turisticosC04);
+
+    printf(" - Resultado D01 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoD01, areaD01, pibD01,turisticosD01);
+    printf(" - Resultado D02 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoD02, areaD02, pibD02,turisticosD02);
+    printf(" - Resultado D03 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoD03, areaD03, pibD03,turisticosD03);
+    printf(" - Resultado D04 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoD04, areaD04, pibD04,turisticosD04);
+
+    printf(" - Resultado E01 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoE01, areaE01, pibE01,turisticosE01);
+    printf(" - Resultado E02 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoE02, areaE02, pibE02,turisticosE02);
+    printf(" - Resultado E03 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoE03, areaE03, pibE03,turisticosE03);
+    printf(" - Resultado E04 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoE04, areaE04, pibE04,turisticosE04);
+   
+    printf(" - Resultado F01 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoF01, areaF01, pibF01,turisticosF01);
+    printf(" - Resultado F02 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoF02, areaF02, pibF02,turisticosF02);
+    printf(" - Resultado F03 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoF03, areaF03, pibF03,turisticosF03);
+    printf(" - Resultado F04 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoF04, areaF04, pibF04,turisticosF04);
+   
+    printf(" - Resultado G01 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoG01, areaG01, pibG01,turisticosG01);
+    printf(" - Resultado G02 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoG02, areaG02, pibG02,turisticosG02);
+    printf(" - Resultado G03 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoG03, areaG03, pibG03,turisticosG03);
+    printf(" - Resultado G04 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoG04, areaG04, pibG04,turisticosG04);
+   
+    printf(" - Resultado H01 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoH01, areaH01, pibH01,turisticosH01);
+    printf(" - Resultado H02 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoH02, areaH02, pibH02,turisticosH02);
+    printf(" - Resultado H03 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoH03, areaH03, pibH03,turisticosH03);
+    printf(" - Resultado H04 \n - População: %f \n - Área: %f \n - PIB: %f \n - Pontos Turisticos %d \n", populacaoH04, areaH04, pibH04,turisticosH04);
+    
+
+
+
+    return 0;
+}
